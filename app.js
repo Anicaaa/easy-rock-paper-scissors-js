0b5d8d1b375c1b2932874729ca25aabff9fbc9a1
Anica Rahman <anicarahman2009@yahoo.it>
2022-10-31 18:13:49 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const yourChoiceDisplay = document.getElementById("your-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
+let yourChoice;
+let computerChoice;
+
+// Create a function with an addEventListener for buttons
+// e.target.id
+// update innerHTML
+// link the second function of computerchoice
+
+// Create a function
+// Math floor and Math random * 3
+// Conditional statement
+// 1. if pcChoose === 1 then return "rock"
+// 2. if pcChoose === 2 then return "scissors"
+// 3. if pcChoose === 3 then return "paper"
